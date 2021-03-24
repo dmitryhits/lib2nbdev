c73892306094b67e03930538174866c307854032
Zachary Mueller <zmueller@novetta.com>
2021-03-24 00:47:27 +0000

    Add in settings.ini generation

--- lib2nbdev/_nbdev.py
+++ lib2nbdev/_nbdev.py
@@ -7,6 +7,7 @@ index = {"start_cell": "00_convert.ipynb",
          "write_cell": "00_convert.ipynb",
          "finish_nb": "00_convert.ipynb",
          "write_nb": "00_convert.ipynb",
+         "generate_settings": "00_convert.ipynb",
          "convert_lib": "00_convert.ipynb"}
 
 modules = ["convert.py"]
